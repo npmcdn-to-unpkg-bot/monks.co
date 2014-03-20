0c84156f029daa2c95fc9f3c2bcc0322f171ae49
Andrew Monks <a@monks.co>
2014-03-20 03:16:50 -0500

    fancy menu

--- app.js
+++ app.js
@@ -44,37 +44,10 @@ $(document).ready(function() {
 		$('#menu').append(  "<li><a href='#" + $(this).parent().parent().attr('id') + "'> " + $(this).text() );
 	})
 
-	
-	var $menu = $('nav#menu'),
-		$html = $('html, body');
+
+	var $menu = $('nav#menu');
 
 	$menu.mmenu();
-	$menu.find( 'li > a' ).on(
-		'click',
-		function()
-		{
-			var href = $(this).attr( 'href' );
-
-			//	if the clicked link is linked to an anchor, scroll the page to that anchor 
-			if ( href.slice( 0, 1 ) == '#' )
-			{
-				$menu.one(
-					'closed.mm',
-					function()
-					{
-						setTimeout(
-							function()
-							{
-								$html.animate({
-									scrollTop: $( href ).offset().top
-								});	
-							}, 10
-						);	
-					}
-				);						
-			}
-		}
-	);
 });
 
 
