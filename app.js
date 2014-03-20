5e24a01cd0b3445dc3ce585a26f862211b1a7314
Andrew Monks <a@monks.co>
2014-03-20 03:02:05 -0500

    fancy menu

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
 // app.js    
 
 
-// smooth scroller
-
-$(document).ready(function() {
-
-  $(document).on('click', 'a[href*="#"]', function() {
-    var slashedHash = '#/' + this.hash.slice(1);
-    if ( this.hash ) {
-
-      if ( slashedHash === location.hash ) {
-        $.smoothScroll({scrollTarget: this.hash});
-      } else {
-        $.bbq.pushState(slashedHash);
-      }
-
-      return false;
-    }
-  });
-
-  $(window).bind('hashchange', function(event) {
-    var tgt = location.hash.replace(/^#\/?/,'');
-    if ( document.getElementById(tgt) ) {
-      $.smoothScroll({scrollTarget: '#' + tgt});
-    }
-  });
-
-  $(window).trigger('hashchange');
-});
-
-
 
 // lazy loader
 
