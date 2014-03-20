5a5e784648b96dd46bba8bcb444d2e16e1aa58a9
Andrew Monks <a@monks.co>
2014-03-20 04:14:19 -0500

    menu

--- app.js
+++ app.js
@@ -48,6 +48,32 @@ $(document).ready(function() {
 	var $menu = $('#menu');
 
 	$menu.mmenu();
+	$menu.find( 'li > a' ).on(
+		'click',
+		function()
+		{
+			var href = $(this).attr( 'href' );
+
+			//	if the clicked link is linked to an anchor, scroll the page to that anchor 
+			if ( href.slice( 0, 1 ) == '#' )
+			{
+				$menu.one(
+					'closed.mm',
+					function()
+					{
+						setTimeout(
+							function()
+							{
+								$html.animate({
+									scrollTop: $( href ).offset().top
+								});	
+							}, 10
+						);	
+					}
+				);						
+			}
+		}
+	);
 });
 
 
